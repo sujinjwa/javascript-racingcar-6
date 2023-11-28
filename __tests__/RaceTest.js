593a842f8949e35b9b534112515fe8c8243e4f0b
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-11-28 19:45:48 +0900

    test: 최종 우승자 구하는 기능 테스트

---
+++ __tests__/RaceTest.js
@@ -0,0 +1,35 @@
+import Race from '../src/domains/Race.js';
+
+describe('Race 클래스 테스트', () => {
+  test('최종 우승자를 구한다.', async () => {
+    // given
+    const race = new Race();
+    const names = ['pobi', 'sujin', 'james', 'jason'];
+    const randomNumbers = [1, 1, 4, 4];
+    race.setCars(names);
+
+    // when
+    race.moveCars(randomNumbers);
+
+    // then
+    const winners = race.calculateWinner();
+    const winnerNames = winners.map((winner) => winner.get()[0]);
+    expect(winnerNames).toStrictEqual(['james', 'jason']);
+  });
+
+  test('최종 우승자를 구한다.', async () => {
+    // given
+    const race = new Race();
+    const names = ['pobi', 'sujin', 'james', 'jason'];
+    const randomNumbers = [1, 8, 3, 2];
+    race.setCars(names);
+
+    // when
+    race.moveCars(randomNumbers);
+
+    // then
+    const winners = race.calculateWinner();
+    const winnerNames = winners.map((winner) => winner.get()[0]);
+    expect(winnerNames).toStrictEqual(['sujin']);
+  });
+});
