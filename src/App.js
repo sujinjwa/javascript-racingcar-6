95e5307af47af91cb7db0ec2b2b75d3014575c33
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-11-28 20:03:00 +0900

    feat: 시도할 횟수 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import InputView from './view/InputView.js';
 class App {
   async play() {
     const cars = await InputView.readCars();
+    const tryNumber = await InputView.readTryNumber();
   }
 }
 
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -6,7 +6,13 @@ const InputView = {
   async readCars() {
     const cars = await Console.readLineAsync(MESSAGE.INPUT_CAR);
 
-    return cars;
+    return cars.split(',');
+  },
+
+  async readTryNumber() {
+    const tryNumber = await Console.readLineAsync(MESSAGE.INPUT_TRY_NUMBER);
+
+    return Number(tryNumber);
   },
 };
 
